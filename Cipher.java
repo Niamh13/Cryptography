a29b8e3894627446442658ee62bc8f563f415d79
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-11-13 17:10:59 +0000

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -19,26 +19,26 @@ public class Cipher {
             {'Z', 'Y', 'X', 'W', 'V', 'U', '*', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', '#', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A', '£'}, 
             {'O', '}', 'U', 'W', 'F', '@', 'S', 'V', 'E', 'I', 'A', 'J', 'X', 'B', 'K', 'D', 'C', 'L', 'Y', '~', 'M', 'N', 'T', 'Q', 'R', 'P', 'R'}, 
             {'A', 'R', 'Q', 'S', 'P', 'F', 'B', '=', 'N', 'C', 'T', 'M', 'D', 'U', 'V', 'K', '+', 'W', 'J', 'X', 'I', 'F', 'Y', 'G', '-', 'H', 'Z'}, 
-            {'S', 'X', 'T', 'U', 'A', 'I', 'Z', 'V', 'B', 'K', 'W', 'H', 'J', 'C', 'Y', 'L', 'D', 'M', 'N', 'E', 'G', 'O', 'F', 'P', 'Q', 'R', '&'}, 
-            {'O', 'N', 'E', 'F', 'M', 'D', 'L', 'G', 'C', 'H', 'Z', 'V', 'B', 'I', 'U', 'Y', 'T', 'K', 'J', 'A', 'S', 'R', 'X', 'Q', 'W', 'P', '!'},
-            {'T', 'U', 'M', 'A', 'S', 'O', 'N', 'I', 'E', 'L', 'Z', 'Y', 'X', 'W', 'R', 'Q', 'P', 'V', 'J', 'K', 'H', 'G', 'F', 'D', 'C', 'B', '?'}, 
-            {'U', 'R', 'G', 'A', 'Y', 'Z', 'W', 'X', 'L', 'K', 'J', 'I', 'M', 'D', 'C', 'B', 'E', 'Q', 'F', 'P', 'S', 'H', 'T', 'O', 'V', 'N', '¬'}, 
-            {'H', 'I', 'O', 'R', 'P', 'T', 'Q', 'V', 'X', 'A', 'J', 'K', 'B', 'L', 'Z', 'C', 'M', 'G', 'N', 'D', 'F', 'U', 'E', 'Y', 'W', 'S', '+'},
+            {'S', 'X', 'T', '+', 'A', 'I', 'Z', 'V', 'B', 'K', 'W', 'H', 'J', 'C', 'Y', 'L', 'D', '=', 'N', 'E', 'G', 'O', 'F', 'P', 'Q', 'R', '&'}, 
+            {'O', 'N', 'E', 'F', 'M', 'D', '-', 'G', 'C', 'H', 'Z', 'V', 'B', 'I', 'U', 'Y', 'T', 'K', 'J', 'A', 'S', 'R', 'X', 'Q', 'W', 'P', '!'},
+            {'T', 'U', 'M', 'A', 'S', 'O', 'N', 'I', 'E', 'L', 'Z', '+', 'X', 'W', 'R', 'Q', 'P', 'V', 'J', 'K', 'H', 'G', 'F', 'D', 'C', 'B', '?'}, 
+            {'U', '0', 'G', '^', 'Y', 'Z', 'W', 'X', 'L', 'K', 'J', 'I', 'M', 'D', 'C', 'B', 'E', '{', 'F', 'P', 'S', 'H', 'T', 'O', 'V', 'N', '¬'}, 
+            {'H', 'I', 'O', 'R', '7', 'T', 'Q', 'V', 'X', '[', 'J', 'K', 'B', 'L', 'Z', 'C', 'M', 'G', 'N', 'D', 'F', 'U', 'E', 'Y', 'W', 'S', '+'},
             {'T', 'O', 'B', 'Y', 'V', 'X', 'Z', 'W', 'D', 'R', 'E', 'N', 'A', 'F', 'J', 'G', 'M', 'U', 'Q', 'C', 'P', 'K', 'H', 'S', 'I', 'L', '-'}, 
             {'J', 'I', 'M', 'D', 'T', 'U', 'W', 'E', 'L', 'N', 'F', 'V', 'S', 'X', 'B', 'O', 'G', 'Y', 'A', 'P', 'H', 'C', 'Q', 'K', 'R', 'Z', '['}, 
-            {'C', 'L', 'A', 'I', 'R', 'E', 'P', 'Z', 'W', 'S', 'X', 'B', 'M', 'Y', 'V', 'Q', 'D', 'T', 'F', 'U', 'O', 'G', 'K', 'J', 'H', 'N', ']'}, 
-            {'B', 'L', 'I', 'N', 'K', 'Y', 'U', 'Q', 'X', 'O', 'W', 'R', 'S', 'E', 'P', 'J', 'D', 'H', 'Z', 'A', 'F', 'M', 'C', 'G', 'V', 'T', '{'}, 
+            {'C', 'L', 'A', 'I', 'R', '%', 'P', 'Z', 'W', 'S', 'X', 'B', 'M', 'Y', '}', 'Q', 'D', 'T', 'F', 'U', '3', 'G', 'K', 'J', 'H', 'N', ']'}, 
+            {'B', 'L', 'I', 'N', 'K', 'Y', 'U', 'Q', '8', 'O', 'W', 'R', 'S', 'E', 'P', 'J', 'D', 'H', 'Z', 'A', 'F', 'M', 'C', 'G', 'V', 'T', '{'}, 
             {'M', 'E', 'R', 'L', 'I', 'N', 'Z', 'T', 'G', 'O', 'U', 'A', 'Y', 'S', 'B', 'H', 'V', 'C', 'K', 'J', 'D', 'X', 'F', 'P', 'W', 'Q', '}'},
-            {'Q', 'X', 'Y', 'J', 'R', 'A', 'S', 'K', 'L', 'B', 'T', 'I', 'M', 'C', 'N', 'H', 'U', 'D', 'G', 'Z', 'E', 'V', 'O', 'F', 'P', 'W', '_'}, 
-            {'H', 'Q', 'M', 'A', 'W', 'R', 'B', 'X', 'I', 'S', 'N', 'Y', 'C', 'K', 'T', 'D', 'O', 'U', 'E', 'J', 'Z', 'F', 'P', 'L', 'V', 'G', '('},
-            {'D', 'F', 'H', 'K', 'L', 'N', 'P', 'R', 'B', 'T', 'V', 'C', 'X', 'Y', 'W', 'Z', 'U', 'S', 'Q', 'O', 'A', 'M', 'J', 'I', 'G', 'E', ')'}, 
-            {'A', 'Z', 'B', 'Y', 'C', 'W', 'D', 'X', 'E', 'V', 'F', 'U', 'G', 'T', 'H', 'S', 'I', 'R', 'K', 'Q', 'J', 'P', 'L', 'O', 'M', 'N', '@'}, 
+            {'Q', 'X', 'Y', 'J', '+', 'A', 'S', 'K', 'L', 'B', 'T', 'I', 'M', 'C', 'N', 'H', 'U', 'D', 'G', 'Z', 'E', 'V', 'O', 'F', 'P', 'W', '_'}, 
+            {'H', ')', 'M', 'A', 'W', 'R', 'B', 'X', 'I', '\', 'N', 'Y', 'C', 'K', 'T', 'D', 'O', 'U', 'E', 'J', '+', 'F', 'P', 'L', 'V', 'G', '('},
+            {'D', 'F', 'H', 'K', 'L', 'N', 'P', '-', 'B', 'T', 'V', 'C', 'X', 'Y', 'W', 'Z', 'U', 'S', 'Q', 'O', 'A', 'M', 'J', 'I', 'G', 'E', ')'}, 
+            {'A', 'Z', 'B', 'Y', 'C', 'W', 'D', 'X', 'E', 'V', 'F', 'U', 'G', 'T', 'H', 'S', 'I', 'R', 'K', 'Q', 'J', '$', 'L', 'O', 'M', 'N', '@'}, 
             {'I', 'D', 'S', 'F', 'R', 'W', 'Q', 'Y', 'X', 'B', 'Z', 'V', 'U', 'E', 'J', 'T', 'K', 'P', 'A', 'O', 'G', 'N', 'M', 'C', 'L', 'H', '~'}, 
-            {'C', 'N', 'M', 'A', 'E', 'O', 'B', 'S', 'V', 'Z', 'Y', 'H', 'W', 'X', 'I', 'T', 'G', 'Q', 'K', 'P', 'R', 'U', 'D', 'J', 'L', 'F', '#'}, 
-            {'K', 'V', 'A', 'J', 'B', 'W', 'U', 'C', 'L', 'X', 'D', 'T', 'M', 'E', 'Z', 'N', 'F', 'Y', 'O', 'S', 'H', 'P', 'G', 'Q', 'I', 'R', '?'}, 
+            {'C', 'N', 'M', 'A', '2', 'O', 'B', 'S', 'V', 'Z', 'Y', 'H', 'W', 'X', 'I', 'T', '£', 'Q', 'K', 'P', 'R', 'U', 'D', 'J', 'L', 'F', '#'}, 
+            {'K', 'V', 'A', 'J', 'B', 'W', 'U', 'C', 'L', '4', 'D', 'T', 'M', 'E', 'Z', 'N', 'F', 'Y', 'O', 'S', 'H', 'P', 'G', 'Q', 'I', 'R', '?'}, 
             {'O', 'F', 'Q', 'W', 'D', 'M', 'N', 'V', 'A', 'L', 'C', 'R', 'K', 'U', 'T', 'B', 'X', 'J', 'G', 'H', 'Z', 'S', 'Y', 'E', 'I', 'P', '!'}, 
             {'B', 'Y', 'E', 'G', 'A', 'N', 'P', 'O', 'C', 'R', 'T', 'D', 'J', 'Z', 'L', 'K', 'X', 'F', 'V', 'W', 'I', 'U', 'H', 'S', 'M', 'Q', '&'}, 
-            {'H', 'K', 'O', 'A', 'Q', 'V', 'B', 'S', 'X', 'M', 'C', 'W', 'U', 'Z', 'D', 'Y', 'L', 'T', 'E', 'R', 'P', 'F', 'N', 'J', 'G', 'I', '$'}};
+            {'H', 'K', '5', 'A', 'Q', 'V', 'B', 'S', 'X', '8', 'C', 'W', 'U', 'Z', 'D', 'Y', 'L', 'T', 'E', '2', 'P', 'F', 'N', '[', 'G', 'I', '$'}};
         
         String[] guessWord = {"Apple of my Eye", "Salt Everything", "Campgrounds", "Ghostbuster", "Cash is King", "Deviously", "Needle in a Haystack", "Recyclables", "Cat Got Your Tongue", "Indubitably", "Pressurised", "Misquoted", "Nightmares", "Shipwrecked", "Brainstormed Ideas", "Sculptured", "Haemoglobin", "Drumsticks Set", "Methodology Terms", "Exemplified", "Xylophone", "Seven Years of Bad Luck", "Haemoneurothorax", "Vibrantly", "Doomed people never blame themselves", "People in Glasshouse should not through stones"};
         
@@ -54,7 +54,7 @@ public class Cipher {
                 
                 for(int level = 1; play == true; level++){
                     //example word 1
-                    String example1E = "Bakers Quiz";
+                    String example1E = "Baker Quiz";
                     StringBuilder example1 = new StringBuilder(example1E);
                     //changing each character
                     for (int s = 0; s < example1.length(); s++) {
@@ -71,7 +71,7 @@ public class Cipher {
                     }
 
                     //example word 2
-                    String example2E = "Clothes";
+                    String example2E = "Cloth";
                     StringBuilder example2 = new StringBuilder(example2E);
                     //changing each character
                     for (int s = 0; s < example2.length(); s++) {
@@ -88,7 +88,7 @@ public class Cipher {
                     }
 
                     //example word 3
-                    String example3E = "Jumpy Fox";
+                    String example3E = "Jumping Fox";
                     StringBuilder example3 = new StringBuilder(example3E);
                     //changing each character
                     for (int s = 0; s < example3.length(); s++) {
