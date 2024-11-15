30844d93367bf02918aa1b4c26c196f9d8a8deec
Ata Turkoglu <116079184+TurkogluAta@users.noreply.github.com>
2024-11-15 19:11:26 +0000

    Update Main.java

--- gardai/src/main/java/com/mycompany/messenger/Main.java
+++ gardai/src/main/java/com/mycompany/messenger/Main.java
@@ -13,9 +13,6 @@ import javax.swing.JOptionPane;
 public class Main {
     
     public static void main(String args[]) {
-        String input = JOptionPane.showInputDialog("Enter Game: ");
-        if(input.equalsIgnoreCase("game")) {
-            Cipher.Play();
-        }
+        Cipher.Play();
     }
 }
